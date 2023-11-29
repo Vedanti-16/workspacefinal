a3a13a8beb94cb57752e4deee11d73c8d52d51be
Iamneo <techadmin@iamneo.ai>
2023-11-29 10:44:38 +0000

    Backup Commit

--- angularapp/src/app/view-enquiry/view-enquiry.component.ts
+++ angularapp/src/app/view-enquiry/view-enquiry.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
-  selector: 'app-view-enquiry',
+  selector: 'app-view-enquiries',
   templateUrl: './view-enquiry.component.html',
   styleUrls: ['./view-enquiry.component.css']
 })
 export class ViewEnquiryComponent implements OnInit {
+  enquiries: any[] = [
+    { id: 1, title: 'Enquiry 1', description: 'Description 1' },
+    { id: 2, title: 'Enquiry 2', description: 'Description 2' },
+    // Add more sample data or replace it with actual data from your API
+  ];
 
   constructor() { }
 
   ngOnInit() {
   }
-
 }
+
