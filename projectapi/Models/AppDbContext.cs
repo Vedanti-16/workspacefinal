135bf3716b509aca42fcb530b66e3af17a8912fc
Iamneo <techadmin@iamneo.ai>
2023-11-30 14:53:18 +0000

    Backup Commit

---
+++ projectapi/Models/AppDbContext.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace projectapi.Models
+{
+    public class AppDbContext
+    {
+        
+    }
+}
