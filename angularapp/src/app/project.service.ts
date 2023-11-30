4b58c177163c3fb8757f49f069222e0704a8b4b3
Iamneo <techadmin@iamneo.ai>
2023-11-30 08:23:29 +0000

    Backup Commit

--- angularapp/src/app/project.service.ts
+++ angularapp/src/app/project.service.ts
@@ -1,4 +1,5 @@
 import { HttpClient } from '@angular/common/http';
+import { identifierModuleUrl } from '@angular/compiler';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -7,9 +8,28 @@ import { Observable } from 'rxjs';
 })
 export class ProjectService {
 
-  constructor(private http:HttpClient) { }
+  private apiURL = "";
 
-  getAllCourses() : Observable<any>{
-    
+  constructor(private http: HttpClient) { }
+
+  getAllCourses(): Observable<any> {
+    return this.http.get<any>(this.apiURL);
+  }
+
+  saveCourseByAdmin(courseData: any): Observable<any> {
+    return this.http.post<any>(this.apiURL,courseData);
+  }
+
+  deleteCourseByAdmin(courseId: number): Observable<any> {
+    return this.http.delete<any>(this.apiURL);
+  }``
+
+  updateCourseByAdmin(courseId: number, courseData: any): Observable<any> {
+    return this.http.put<any>(this.apiURL+"/"+courseId,courseData);
+  }
+
+
+  getStudentCourses(): Observable<any>{
+    return this.http.get<any>(this.apiURL)
   }
 }
