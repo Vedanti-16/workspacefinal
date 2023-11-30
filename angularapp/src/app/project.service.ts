b2c2a0bbb3be028c3c288521eaf8df65c6581258
Iamneo <techadmin@iamneo.ai>
2023-11-30 07:08:41 +0000

    Backup Commit

--- angularapp/src/app/project.service.ts
+++ angularapp/src/app/project.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getAllCourses() : Observable<any>{
+    
+  }
 }
