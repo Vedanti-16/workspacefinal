4cc0b6c2577d793f736a64adf918803e8ce92790
Iamneo <techadmin@iamneo.ai>
2023-12-01 10:13:21 +0000

    Backup Commit

--- angularapp/src/app/project.service.ts
+++ angularapp/src/app/project.service.ts
@@ -8,31 +8,41 @@ import { Observable } from 'rxjs';
 })
 export class ProjectService {
 
-  private apiURL = "";
+  private apiUrl = "";
 
   constructor(private http: HttpClient) { }
 
   //-------------------COURSES ADMIN----------------------------------
 
   getAllCourses(): Observable<any> {
-    return this.http.get<any>(this.apiURL);
+    return this.http.get<any>(`${this.apiUrl}/api/course`);
   }
 
   saveCourseByAdmin(courseData: any): Observable<any> {
-    return this.http.post<any>(this.apiURL,courseData);
+    return this.http.post<any>(`${this.apiUrl}/api/course`,courseData);
   }
 
   deleteCourseByAdmin(courseId: number): Observable<any> {
-    return this.http.delete<any>(this.apiURL);
+    return this.http.delete<any>(`${this.apiUrl}/api/course/${courseId}`);
   }
 
   updateCourseByAdmin(courseId: number, courseData: any): Observable<any> {
-    return this.http.put<any>(this.apiURL+"/"+courseId,courseData);
+    return this.http.put<any>(`${this.apiUrl}/api/course/${courseId}`,courseData);
   }
 
   getStudentCourses(): Observable<any>{
-    return this.http.get<any>(this.apiURL)
+    return this.http.get<any>(`${this.apiUrl}/api/student/course`)
   }
 
   //-------------------------
+
+  getAllEnquiriesByAdmin() : Observable<any>{
+    return this.http.get<any>(`${this.apiUrl}/api/enquiry`);
+  }
+
+  getEnquiryByAdmin(enquiryId: number): Observable<any>{
+    return this.http.get<any>(`${this.apiUrl}/api/enquiry/${enquiryId}`);
+  }
+
+
 }
