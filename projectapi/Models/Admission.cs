5ce3bb74292c7da05db022f36e8c7917c2b7b4d7
Iamneo <techadmin@iamneo.ai>
2023-12-02 10:02:03 +0000

    Backup Commit

--- projectapi/Models/Admission.cs
+++ projectapi/Models/Admission.cs
@@ -12,6 +12,11 @@ namespace projectapi.Models
         public int UserId { get; set; }
         public string? Status { get; set; }
         public virtual User? User { get; set; }
+        public virtual Course? Course {get;set;}
+        public string? CreatedBy {get; set;}
+        public DateTime CreatedAt {get;set;}
+        public string? ModifiedBy {get; set;}
+        public DateTime ModifiedAt {get;set;}
         public Boolean IsDeleted { get; set; }
     }
 }
